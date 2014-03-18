f52296dd8095619901b1fa12a07b2bc1a15105ae
sebastian cole <sebastian.cole@mi9.com.au>
2014-03-19 09:37:57 +1100

    Implement submission endpoint + switchable backends
    now listening to /candidates to trigger tests, nodes objects have been extracted out to make it easier to simulate a failure.

--- main.go
+++ main.go
@@ -1,31 +1,65 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"github.com/codegangsta/martini"
 	"sync"
 )
 
 type Node struct {
 	Port string
+	Available bool
 }
 
-func (n Node) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "OK from%s - Unique res: %s", n.Port, r.URL.Path[1:])
+func (n *Node) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if n.Available {
+		fmt.Fprintf(w, "OK from%s - Unique res: %s - %t", n.Port, r.URL.Path[1:], n.Available)
+		n.Available = false
+	} else {
+		http.Error(w, "Backend node failed", 500)
+		n.Available = true
+	}
+}
+
+func errorsAsJson(errors []error) string {
+	messages := make([]string, len(errors))
+	for i, err := range errors {
+		println(err.Error())
+		messages[i] = err.Error()
+	}
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(&map[string][]string{"errors": messages})
+	return buf.String()
+}
+
+func mainHandler(r *http.Request, params martini.Params) (responseCode int, body string) {
+	url := r.FormValue("url")
+	email := r.FormValue("email")
+	if url == "" || email == "" {
+		return 400, `{ "error": "'url' and 'email' must be provided" }`
+	}
+	return 501, `{ "error": "not implemented"}`
 }
 
 func main() {
 	//flag.Parse()
-	ports := []string{":8080", ":8081", ":8082"}
+	nodes := []Node{{":8080", true},{":8081", true}}
 	var wg sync.WaitGroup
-	for _, port := range ports {
+	for _, node := range nodes {
 		wg.Add(1)
-		go func(port string) {
-			var node = &Node{}
-			node.Port = port
-			http.ListenAndServe(port, node)
+		go func(n Node) {
+			http.ListenAndServe(n.Port, &n)
 			wg.Done()
-		}(port)
+		}(node)
 	}
+	m := martini.Classic()
+	m.Put("/candidates", mainHandler)
+	m.Use(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	})
+	m.Run()
 	wg.Wait()
 }
