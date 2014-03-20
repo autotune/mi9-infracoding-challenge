031e14c5bbadbbbbc9aedf09c31b2749ca667f11
Sebastian Cole <sebastian.paris@gmail.com>
2014-03-21 09:18:37 +1100

    Making the backend node messages easier to read.

--- backend.go
+++ backend.go
@@ -12,8 +12,8 @@ type Node struct {
 
 func (n *Node) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if n.Available {
-		fmt.Fprintf(w, "{ \"id\": \"%s\",\"response\": \"%s\" }", n.Port, r.URL.Path[1:])
+		fmt.Fprintf(w, "{ \"id\": \"%s\",\"Message\": \"%s\" }", n.Port, r.URL.Path[1:])
 	} else {
-		http.Error(w, "Backend node failed", 500)
+		http.Error(w, "{ \"id\": \"%s\",\"status\": \"offline\"}", 500)
 	}
 }
