9804b64455d3ea2def27cd99cae4a97f84492cf0
Sebastian Cole <sebastian.paris@gmail.com>
2014-03-21 11:48:06 +1100

    Extracting testing the LB

--- scenario_loadbalancing.go
+++ scenario_loadbalancing.go
@@ -11,6 +11,27 @@ func init() {
 	scenarios = append(scenarios, RunValidScenario)
 }
 
+func TestUrl(url string, client *http.Client) (id string, errors []error) {
+	var validRequestResponse validResponse
+
+	resp, err := client.Get(url)
+	if err != nil {
+		errors = append(errors, err)
+		return "", errors
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		errors = append(errors, fmt.Errorf("Expected status code 200, but got %d", resp.StatusCode))
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&validRequestResponse)
+	if err != nil {
+		errors = append(errors, fmt.Errorf("Cannot parse json: %s", err))
+	}
+
+	return validRequestResponse.Id, errors
+}
+
 func RunValidScenario(url, email string, nodes NodeSlice, client *http.Client) (errors []error) {
 	/*  - Generate a bunch of requests
 	*   - Interpret the requests, pulling out the node id
@@ -20,29 +41,19 @@ func RunValidScenario(url, email string, nodes NodeSlice, client *http.Client) (
 	nodes.Enable()
 	time.Sleep(6 * time.Second)
 	var loadBlancingResults = map[string]int{}
-	var validRequestResponse validResponse
 
 	for _, node := range nodes {
 		loadBlancingResults[node.Port] = 0
 	}
 
 	for i := 0; i < 100; i++ {
-		resp, err := client.Get(url)
+		id, err := TestUrl(url, client)
 		if err != nil {
-			errors = append(errors, err)
-			return
+			errors = append(errors, err...)
 		}
-		//defer resp.Body.Close()
-		if resp.StatusCode != 200 {
-			errors = append(errors, fmt.Errorf("Expected status code 200, but got %d", resp.StatusCode))
+		if id != "" {
+			loadBlancingResults[id] = loadBlancingResults[id] + 1
 		}
-
-		err = json.NewDecoder(resp.Body).Decode(&validRequestResponse)
-		if err != nil {
-			errors = append(errors, fmt.Errorf("Cannot parse json: %s", err))
-		}
-		loadBlancingResults[validRequestResponse.Id] = loadBlancingResults[validRequestResponse.Id] + 1
-		resp.Body.Close()
 	}
 	if loadBlancingResults[nodes[0].Port] != loadBlancingResults[nodes[1].Port] {
 		errors = append(errors, fmt.Errorf("Load wasn't balanced enough, node0 received %d requests and node1 %d", loadBlancingResults[nodes[0].Port], loadBlancingResults[nodes[1].Port]))
